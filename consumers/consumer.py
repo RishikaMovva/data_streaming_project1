c73662ad89094946bcbd26c5baef693309aadbec
RishikaMovva <43386977+RishikaMovva@users.noreply.github.com>
2023-03-09 14:48:36 -0800

    Create consumer.py

---
+++ consumers/consumer.py
@@ -0,0 +1,93 @@
+"""Defines core consumer functionality"""
+import logging
+
+import confluent_kafka
+from confluent_kafka import Consumer
+from confluent_kafka.avro import AvroConsumer
+from confluent_kafka.avro.serializer import SerializerError
+from tornado import gen
+
+
+logger = logging.getLogger(__name__)
+
+
+class KafkaConsumer:
+    """Defines the base kafka consumer class"""
+
+    def __init__(
+        self,
+        topic_name_pattern,
+        message_handler,
+        is_avro=True,
+        offset_earliest=False,
+        sleep_secs=1.0,
+        consume_timeout=0.1,
+    ):
+        """Creates a consumer object for asynchronous use"""
+        self.topic_name_pattern = topic_name_pattern
+        self.message_handler = message_handler
+        self.sleep_secs = sleep_secs
+        self.consume_timeout = consume_timeout
+        self.offset_earliest = offset_earliest
+
+        #
+        #
+        # TODO: Configure the broker properties below. Make sure to reference the project README
+        # and use the Host URL for Kafka and Schema Registry!
+        #
+        #
+        self.broker_properties = {
+            'bootstrap.servers': 'PLAINTEXT://localhost:9092',
+            'group.id': f"{self.topic_name_pattern}",
+            'default.topic.config': {"acks":"all", "auto.offset.reset": "earliest"}
+        }
+
+        # TODO: Create the Consumer, using the appropriate type.
+        if is_avro is True:
+            self.broker_properties["schema.registry.url"] = "http://localhost:8081"
+            self.consumer = AvroConsumer(self.broker_properties)
+        else:
+            self.consumer = Consumer(self.broker_properties)
+
+        self.consumer.subscribe([self.topic_name_pattern], on_assign=self.on_assign)
+
+    def on_assign(self, consumer, partitions):
+        """Callback for when topic assignment takes place"""
+        for partition in partitions:
+            partitions.offset = confluent_kafka.OFFSET_BEGINNING
+        logger.info("partitions assigned for %s", self.topic_name_pattern)
+        consumer.assign(partitions)
+
+    async def consume(self):
+        """Asynchronously consumes data from kafka topic"""
+        while True:
+            num_results = 1
+            logger.info("one result received from kafka topic")
+            while num_results > 0:
+                num_results = self._consume()
+            logger.info("more than one results received from kafka topic")
+            await gen.sleep(self.sleep_secs)
+
+    def _consume(self):
+        """Polls for a message. Returns 1 if a message was received, 0 otherwise"""
+        try:
+            message = self.consumer.poll(timeout=1.0)
+            if message is not None:
+                if message.error() is not None:
+                    self.message_handler(message)
+                    return 1
+                elif message.error().code() != KafkaError._PARTITION_EOF:
+                    logger.error(message.error())
+                    return 0
+            else:
+                logger.debug("no message received by consumer")
+                return 0
+        except SerializerError as er:
+            logger.error(f"Error while consuming data: {er.message}")
+            return 0
+
+
+    def close(self):
+        """Cleans up any open kafka consumers"""
+        logger.info("Closing the consumer")
+        self.consumer.close()
